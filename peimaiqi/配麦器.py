59e654959426c12d34df871733707776fbc62753
mobilebook <sxmgame@163.com>
2020-04-04 21:52:32 +0800

    2020.4.4 21:52

--- peimaiqi/配麦器.py
+++ peimaiqi/配麦器.py
@@ -173,12 +173,12 @@ class Mymainform(QMainWindow, Ui_MainWindow):
             except Exception as e:
                 print(e)
                 mywin.label_38.setText("设置失败")
-    def qingbaojing(self):#1号秤清报警
+    def qingbaojing1(self):#1号秤清报警
         try:
             if (ser.is_open):
                 str = '0105000eff00edf9'  # 清除1号秤报警，2号秤0205000eff00edca，3号秤0305000eff00ec1b
                 DWritePort(ser, str)
-                time.sleep(0.5)
+                #time.sleep(0.5)
                 STRGL = str(binascii.b2a_hex(ser.readline()))[2:-1]
                 print(STRGL)
                 if STRGL.__eq__(str):
@@ -188,6 +188,36 @@ class Mymainform(QMainWindow, Ui_MainWindow):
         except Exception as e:
             print(e)
             mywin.label_34.setText("清除失败")
+    def qingbaojing2(self):#2号秤清报警
+        try:
+            if (ser.is_open):
+                str = '0205000eff00edca'  # 清除2号秤报警
+                DWritePort(ser, str)
+                #time.sleep(0.5)
+                STRGL = str(binascii.b2a_hex(ser.readline()))[2:-1]
+                print(STRGL)
+                if STRGL.__eq__(str):
+                    mywin.label_36.setText("清除成功")
+                else:
+                    mywin.label_36.setText("清除失败")
+        except Exception as e:
+            print(e)
+            mywin.label_36.setText("清除失败")
+    def qingbaojing3(self):#3号秤清报警
+        try:
+            if (ser.is_open):
+                str = '0305000eff00ec1b'  # 清除3号秤报警
+                DWritePort(ser, str)
+                #time.sleep(0.5)
+                STRGL = str(binascii.b2a_hex(ser.readline()))[2:-1]
+                print(STRGL)
+                if STRGL.__eq__(str):
+                    mywin.label_38.setText("清除成功")
+                else:
+                    mywin.label_38.setText("清除失败")
+        except Exception as e:
+            print(e)
+            mywin.label_38.setText("清除失败")
     def yuebaobiao(self):  #
         pass
     def banzubaobiao(self):  #
@@ -204,68 +234,62 @@ class Mymainform(QMainWindow, Ui_MainWindow):
 # 串口模块区域
 import threading, datetime, time, struct, binascii
 
-STRGLO = ""  # 读取的累计流量
-STRGLO1 = ""  # 读取的瞬时流量
-STRGLO2 = ""  # 读取的目标流量
-STRGLO3 = ""  #读取状态
+STRGLO1 = ""  # 暂存1号秤上次累计流量
+STRGLO2 = ""  # 暂存2号秤上次累计流量
 BOOL = True  # 读取标志位
 # 读数代码本体实现
 def ReadData(ser):
-    global STRGLO,STRGLO1,STRGLO2,BOOL,dakai
+    global STRGLO1,STRGLO2
     # 循环接收数据，此为死循环，可用线程实现
-    while BOOL:# and dakai is True:
+    while BOOL:
         hour = time.localtime().tm_hour
         now = datetime.datetime.now()
         if (ser.is_open):
-            #1号秤信息
-            mubiao1=readserial(ser,'010300000002C40B')#读取1号秤目标流量
-            mywin.label_11.setText(str(mubiao1))#显示1号秤目标流量
-            leiji1=readserial(ser,'01030008000245C9')#读取1号秤累计流量
-            mywin.label_3.setText(str(leiji1))  # 显示1号秤累计流量
-            shunshi1=readserial(ser,'0103000A0002E409')#读取1号瞬时计流量
-            mywin.label_5.setText(str(shunshi1))  # 显示1号秤瞬时流量
-            DWritePort(ser, '010400100002700')  # 读取状态
-            zhuangtai1 = str(binascii.b2a_hex(ser.readline()))[11:12]
-            mywin.label_34.setText(baojing(zhuangtai1))  # 调用baojing.py中的报警信息处理函数，并返回值。
-
-
-'''
-            DWritePort(ser, str1)#读取目标流量
-            time.sleep(1)
-            STRGLO2 = str(binascii.b2a_hex(ser.readline()))[2:-1]
-
-            DWritePort(ser, str2)#读取累计流量
-            time.sleep(1)
-            STRGLO = str(binascii.b2a_hex(ser.readline()))[2:-1]
-
-            DWritePort(ser, str3)#读取瞬时流量
-            time.sleep(1)
-            STRGLO1 = str(binascii.b2a_hex(ser.readline()))[2:-1]
-            print(STRGLO1)
-
-            DWritePort(ser, str4)#读取状态
-            time.sleep(1)
-            STRGLO3 = str(binascii.b2a_hex(ser.readline()))[11:12]
-            print(STRGLO3)
-            mywin.label_34.setText(baojing(STRGLO3))#调用baojing.py中的报警信息处理函数，并返回值。
-            '''
             try:
-                s = "INSERT INTO liuliang VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')"
-                sqlstr=s.format('一厂区三车间',班组,now.strftime("%Y-%m-%d"),now.strftime("%H:%M:%S"),STRGLO1[0:2],flnum,flnum1,0)
-                if not STRGLO3.__eq__('5'):#只要不是停止运行就写数据库。
+                #1号秤信息
+                mubiao1=readserial(ser,'010300000002C40B')#读取1号秤目标流量
+                mywin.label_11.setText(str(mubiao1))#显示1号秤目标流量
+                leiji1=readserial(ser,'01030008000245C9')#读取1号秤累计流量
+                STRGLO1=leiji1#暂存入全局变量，防止下次设备断电取不到数
+                mywin.label_3.setText(str(leiji1))  # 显示1号秤累计流量
+                shunshi1=readserial(ser,'0103000A0002E409')#读取1号瞬时计流量
+                mywin.label_5.setText(str(shunshi1))  # 显示1号秤瞬时流量
+                DWritePort(ser, '010400100002700e')  # 读取状态
+                zhuangtai1 = str(binascii.b2a_hex(ser.readline()))[11:12]
+                mywin.label_34.setText(baojing(zhuangtai1))  # 调用baojing.py中的报警信息处理函数，并返回值。
+            except Exception as e:
+                mywin.label_3.setText('数据异常！')  # 累计流量
+                mywin.label_5.setText('数据异常！')  # 瞬时流量
+            try:
+                #2号秤信息
+                mubiao2=readserial(ser,'020300000002C438')#读取2号秤目标流量
+                mywin.label_15.setText(str(mubiao2))#显示2号秤目标流量
+                leiji2=readserial(ser,'02030008000245fa')#读取2号秤累计流量
+                STRGLO2 = leiji2  # 暂存入全局变量，防止下次设备断电取不到数
+                mywin.label_17.setText(str(leiji2))  # 显示2号秤累计流量
+                shunshi2=readserial(ser,'0203000A0002E43a')#读取2号瞬时计流量
+                mywin.label_20.setText(str(shunshi2))  # 显示2号秤瞬时流量
+                DWritePort(ser, '020400100002703d')  # 读取状态
+                zhuangtai2 = str(binascii.b2a_hex(ser.readline()))[11:12]
+                mywin.label_36.setText(baojing(zhuangtai2))  # 调用baojing.py中的报警信息处理函数，并返回值。
+            except Exception as e:
+                mywin.label_17.setText('数据异常！')  # 累计流量
+                mywin.label_20.setText('数据异常！')  # 瞬时流量
+            try:
+                if hour>=7 and hour<19:
+                    班组='白班'
+                else:
+                    班组='夜班'
+                s = "INSERT INTO liuliang VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}')"
+                sqlstr=s.format('一厂区四车间',班组,now.strftime("%Y-%m-%d"),now.strftime("%H:%M:%S"),mubiao1[0:2],leiji1,shunshi1,mubiao2[0:2],leiji2,shunshi2,0)
+                if not zhuangtai1.__eq__('5') and zhuangtai2.__eq__('5'):#只要不是两台称都停止运行就写数据库。
                     shujuku.sqlzsg(sqlstr)
                     print('写数据库')
             except Exception as e:
                 print(e)
-                mywin.label_3.setText('数据异常！')#累计流量
-                mywin.label_5.setText('数据异常！')#瞬时流量
         time.sleep(60)
 
 
-str1 = '010300000002C40B'  # 读取目标流量命令
-str2 = '01030008000245C9'  # 读取累计流量
-str3 = '0103000A0002E409'  # 读取瞬时流量
-str4 = '010400100002700E' #读取状态
 str7 = '01100000000204'    # 设置目标流量命令的前面部分，+数据位+校验位即是完整命令。
 
 if __name__ == "__main__":
